392a1f9f9a3b94b3b412669c94fae1e617965185
venugopalreddy123 <venu641995@gmail.com>
2020-09-08 16:53:13 +0530

    name added

--- src/App.js
+++ src/App.js
@@ -3,46 +3,12 @@ import { Input } from "./Input";
 import { Output } from "./Output";
 
 class App extends React.Component {
-  state = {
-    result: "",
-  };
-
-  buttonPressed = (buttonName) => {
-    if (buttonName === "=") {
-      this.calculate();
-    } else if (buttonName === "c") {
-      this.clear();
-    } else if (buttonName === "ce") {
-      this.backSpace();
-    } else
-      this.setState({
-        result: this.state.result + buttonName,
-      });
-  };
-  calculate = () => {
-    this.setState({
-      result: eval(this.state.result),
-    });
-  };
-  clear = () => {
-    this.setState({
-      result: "",
-    });
-  };
-  backSpace = () => {
-    this.setState({
-      result: this.state.result.slice(0, -1),
-    });
-  };
-
   render() {
     return (
       <div className="App">
-        <h1 style={{ textAlign: "center" }}>Calculator</h1>
-        <div className="main-class">
-          <Output result={this.state.result} />
-          <Input buttonPressed={this.buttonPressed} />
-        </div>
+        <Input />
+        <Output />
+        
       </div>
     );
   }
--- src/Input.jsx
+++ src/Input.jsx
@@ -1,72 +1,10 @@
 import React from "react";
-import "./Styles.css";
 export class Input extends React.Component {
-  buttonPressed = (e) => {
-    this.props.buttonPressed(e.target.name);
-  };
   render() {
     return (
-      <div className="input-buttons">
-        <button name="1" onClick={this.buttonPressed}>
-          1
-        </button>
-        <button name="2" onClick={this.buttonPressed}>
-          2
-        </button>
-        <button name="3" onClick={this.buttonPressed}>
-          3
-        </button>
-        <button name="4" onClick={this.buttonPressed}>
-          4
-        </button>
-        <button name="5" onClick={this.buttonPressed}>
-          5
-        </button>
-        <button name="6" onClick={this.buttonPressed}>
-          6
-        </button>
-        <button name="7" onClick={this.buttonPressed}>
-          7
-        </button>
-        <button name="8" onClick={this.buttonPressed}>
-          8
-        </button>
-        <button name="9" onClick={this.buttonPressed}>
-          9
-        </button>
-        <button name="0" onClick={this.buttonPressed}>
-          0
-        </button>
-        <button name="+" onClick={this.buttonPressed}>
-          +
-        </button>
-        <button name="-" onClick={this.buttonPressed}>
-          -
-        </button>
-        <button name="*" onClick={this.buttonPressed}>
-          *
-        </button>
-        <button name="/" onClick={this.buttonPressed}>
-          /
-        </button>
-        <button name="=" onClick={this.buttonPressed}>
-          =
-        </button>
-        <button name="c" onClick={this.buttonPressed}>
-          c
-        </button>
-        <button name="ce" onClick={this.buttonPressed}>
-          ce
-        </button>
-        <button name="%" onClick={this.buttonPressed}>
-          %
-        </button>
-        <button name="(" onClick={this.buttonPressed}>
-          (
-        </button>
-        <button name=")" onClick={this.buttonPressed}>
-          )
-        </button>
+      <div>
+        <h1>Todo App</h1>
+        <h2>Parameswar Reddy</h2>
       </div>
     );
   }
